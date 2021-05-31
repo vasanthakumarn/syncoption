c0e9ba7bad177db30e1e2382b45cadbf15d2134a
vasanthakumarn <41970169+vasanthakumarn@users.noreply.github.com>
2021-05-31 12:47:18 +0530

    Updated environment based build

--- build.sh
+++ build.sh
@@ -1,1 +1,5 @@
-ng build --configuration=production --output-hashing all
+build_production='ng build --configuration=production --output-hashing all'
+if [ $NODE_ENV = "production" ]; then
+ echo "running $build_production ..."
+ eval "$build_production"
+fi
