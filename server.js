2d40bc2c5e7e92b81a614ac5402ed2aeee0a5285
vasanthakumarn <41970169+vasanthakumarn@users.noreply.github.com>
2021-05-31 11:05:08 +0530

    Added Server.js to serve the application

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const path = require("path");
+const httpsRedirect = require('express-https-redirect');
+
+function wwwDotMiddleware() {
+  return function (req, res, next) {
+    if (!req.hostname.includes('herokuapp') && !req.hostname.includes('www')) {
+      res.redirect('https://' + 'www.' + req.hostname + req.originalUrl);
+    } else {
+      next();
+    }
+  }
+}
+
+const app = express();
+
+app.use('/', httpsRedirect());
+
+app.use('/', wwwDotMiddleware());
+
+app.use(express.static(__dirname + '/dist/frontend', {
+  maxAge: '1d',
+  setHeaders: response => {
+    response.setHeader('Expires', new Date(Date.now() + 86400000 * 1).toUTCString());
+  }
+}));
+
+app.get("/*", (request, response) => {
+  response.sendFile(path.join(__dirname + '/dist/frontend/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
